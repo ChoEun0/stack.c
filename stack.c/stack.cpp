f54fa285dcc4f45e06946446b2e69190d903a51c
choeun <eun9861@naver.com>
2023-11-17 19:41:28 +0900

    동적할당 수정하기

--- stack.c/stack.cpp
+++ stack.c/stack.cpp
@@ -1,68 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 // 구조체 선언 -> 초기화 -> 상태확인 -> 더하기 빼기 -> 현상태 확인
-#define SIZE 5
+
 
 typedef struct Stack {
-	int data[SIZE];
+	int size;
 	int top;
+	int* array;
 }Stack;
 
+Stack* createStack(int size) {
+	Stack* s = (Stack*)malloc(sizeof(Stack));
+	if (!s) return NULL;
 
-void init_Stack(Stack* s) {
+	s->size = size;
 	s->top = -1;
+	s->array = (int*)malloc(s->size * sizeof(int));
+	return s;
 }
 
+
 int empty_check(Stack* s) {
-	if (s->top == -1) return 1;
-	else return 0;
+	return (s->top == -1);
 }
 
 int full_check(Stack* s) {
-	if (s->top == SIZE - 1) return 1;
-	else return 0;
+	return (s->top == s->size -1);
 }
 
-void add(Stack* s, int n) {
+void add(struct Stack *s, int n) {
 	if (full_check(s)) {
-		printf("더 이상 입력할 수 없습니다");
-		return;
+		s->size *= 2;
+		s->array = (int*)realloc(s->array, s->size);
+		printf("2배로 늘리기\n");
+
 	}
 	else {
 		s->top++;
-		s->data[s->top] = n;
+		s->array[s->top] = n;
+		printf("%d push to stack\n", n);
 	}
 }
 
 int get(Stack* s) {
 	if (empty_check(s)) {
-		printf("오류발생. 스택이 비어있음. 프로그램을 종료합니다.");
+		printf("오류발생. 스택이 비어있음. 프로그램을 종료합니다.\n");
 		exit(1);
 	}
 	else {
-		return s->data[s->top--];
+		return s->array[s->top--];
 	}
 }
 
 int peek(Stack* s) {
 	if (empty_check(s)) {
-		printf("오류발생. 스택이 비어있음. 프로그램을 종료합니다.");
+		printf("오류발생. 스택이 비어있음. 프로그램을 종료합니다.\n");
 		exit(1);
 	}
 	else{
-		return s->data[s->top];
+		return s->array[s->top];
 	}
 }
 
 int main() {
-	Stack s;
-	init_Stack(&s);
-	int number;
-	printf("숫자를 5개 입력하세요.\n");
-	for (int i = 0; i < 5; i++) {
-		scanf_s("%d\n", &number);
-	}
-	add(&s, number);
-	printf("%d\n", get(&s));
+	Stack* s = createStack(3);
+	
+	add(s, 1);
+	add(s, 2);
+	add(s, 3);
+	add(s, 4);
+
+	printf("%d pop from stack\n", get(s));
 
 }
